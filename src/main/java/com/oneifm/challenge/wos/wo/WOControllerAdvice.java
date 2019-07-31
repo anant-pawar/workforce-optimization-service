0fd7f9a53c8a4663abe192dd720f4a7ee4b84be8
Anant Pawar <anant.c.pawar@gmail.com>
2019-07-31 22:50:18 +0530

    added error logs

--- src/main/java/com/oneifm/challenge/wos/wo/WOControllerAdvice.java
+++ src/main/java/com/oneifm/challenge/wos/wo/WOControllerAdvice.java
@@ -7,15 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.logging.Logger;
+
 @ControllerAdvice(assignableTypes  = WOController.class)
 public class WOControllerAdvice {
+    private final static Logger LOGGER = Logger.getLogger(WOControllerAdvice.class.getName());
+
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<WOError> handleRunTimeException(RuntimeException exception) {
+        LOGGER.severe(exception.getMessage());
+
         return new ResponseEntity<WOError>(new WOError(exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({InvalidInputException.class})
     public ResponseEntity<WOError> handleInvalidInputException(InvalidInputException exception) {
+        LOGGER.severe(exception.getMessage());
+
         return new ResponseEntity<WOError>(new WOError(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
